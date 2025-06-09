584479a1d7e34adc958d2d4dc9a85f420e3bf67a
wardaowais <wardaowais123@gmail.com>
2025-06-09 18:18:50 +0500

    offilne functionality test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 // next.config.mjs
-
 import withPWA from 'next-pwa';
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -9,13 +8,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // Add more Next.js config here if needed
 };
 
-// Wrap and export using withPWA only if NOT in dev
 export default withPWA({
   dest: 'public',
-  disable: isDev, // This disables service worker in dev mode
+  disable: isDev, // Only disable in development
   register: true,
   skipWaiting: true,
 })(nextConfig);
